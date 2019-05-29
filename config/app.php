1431960732eada27348217116d97c471cdb0b483
dev <ibrahimalamine@gmail.com>
2019-05-29 17:17:49 +0000

    modifié :         composer.json 	modifié :         composer.lock 	modifié :         config/app.php

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 	Reliese\Coders\CodersServiceProvider::class,
+	Mpociot\LaravelTestFactoryHelper\TestFactoryHelperServiceProvider::class,
 
     ],
 
